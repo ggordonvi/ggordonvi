cc65b05e01126d7185399f1eb5dc4052eabf29d7
George Gordon <ggordonvi@gmail.com>
2022-03-27 12:01:50 -0400

    Initial Checkin

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    
+    camera.position.z = 5;
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5,5,5);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200, 50); 
+    scene.add(lightHelper, gridHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+
+    function animate() {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      controls.update();
+
+      renderer.render( scene, camera );
+    }
+    animate();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+  }
+
+
+}
