f2f9f441c8841a9a39a97257bbc65aef84266aba
George Gordon <ggordonvi@gmail.com>
2022-03-31 01:54:05 -0400

    Minor changes & filler space/text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  public currentTime = new Date();
+  public greetings = ['good morning', 'good afternoon', 'good evening'];
+  public defaultGreeting = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const currentTime = new Date();
+    // console.log(currentTime.toLocaleDateString("en-US"));
+    // console.log(currentTime.getHours() + ":" + currentTime.getMinutes());
+    if (currentTime.getHours() < 12) {
+      this.defaultGreeting = this.greetings[0];
+    } else if (currentTime.getHours() < 17) {
+      this.defaultGreeting = this.greetings[1];
+    } else {
+      this.defaultGreeting = this.greetings[2];
+    }
+
   }
 
 }
